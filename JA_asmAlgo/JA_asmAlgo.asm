417756451c5b0f28e8c1f50cbf6abe63718e7328
adburz <adam.burzynski18922@gmail.com>
2020-11-02 00:17:11 +0100

    asm and cpp algorithm done

--- JA_asmAlgo/JA_asmAlgo.asm
+++ JA_asmAlgo/JA_asmAlgo.asm
@@ -9,52 +9,86 @@ BMP_BYTE_COUNT_FOR_ONE_CHAR BYTE 24
 .code
 ;============ DECODING ============ 
 decoding PROC
-;uzywane rejestry: RSI(wskaznik na msg) |  | RBX(licznik inner_for) | R15(licznik outer_for) | R8(koniec wiadomosci mEnd)
+	
+	;RCX - bmpArr
+	;RDX - msgArr
+	;R8 - mEnd
 
-	mov RSI, RDX	;zapisuje wskaznik na tablice z wiadomoscia, aby moc uzywac instrukcji mul, ktora potrzebuje rejestru RDX do poprawnego dzialania
-	xor bl, bl		;licznik "i" zapisany w rejestrze al wewnetrznej petli for, ktora w cpp wyglada tak: for(int i=1; i<=24; i+=3)
+	push rbp					; zapisuje adres rejestru RBP
+	push rdi					; zapisuje adres rejestru RDI
+	push rsp					; zapisuje adres rejestru RSP
+
+	mov R9, RDX		;zapisuje wskaznik na tablice z wiadomoscia, aby moc uzywac instrukcji mul, ktora potrzebuje rejestru RDX do poprawnego dzialania
+	xor rbx, rbx	;licznik "i" zapisany w rejestrze al wewnetrznej petli for, ktora w cpp wyglada tak: for(int i=1; i<=24; i+=3)
 	inc bl			;inkrementujê rejestr al, aby docelowo byl rowny 1
 
-	xor r15d,r15d	;licznik zewnetrznej petli for. Petla w cpp wyglada tak: for(int i=0; i<mEnd; i++), gdzie mEnd to wartosc zaladowana do rejestru r15
-
+	xor r15,r15		;licznik zewnetrznej petli for. Petla w cpp wyglada tak: for(int i=0; i<mEnd; i++), gdzie mEnd to wartosc zaladowana do rejestru r15
+	
 ;---------------- OUTER FOR ----------------		
 outer_for:	
-;---------------- INNER FOR ----------------
-		
-		mov r14b, [RSI + R15]	;czytam przy pomocy iteratora zewnetrznej petli (RBX) jeden znak (bajt) z wiadomosci.
-			inner_for:				;petla wyglada w cpp tak: for(int i=1; i<=24; i+=3) poniewaz kodujemy co trzeci bajt pliku BMP -> 1 bajt wiadomosci = 24 bajty pliku bmp.
-					
-					mov r13b, r14b			;zapisuje r13, aby nie stracic bajtu z r14. Bajt zostanie potraktowany przesunieciem bitowym w lewo o 1 z koncem petli. 
-					and r13b, 1				;wykonujê operacjê mod2, aby odczytaæ bit o najmniejszym znaczeniu bajtu, który nastêpnie zostanie zakodowany w obrazie. 
+;---------------- INNER FOR ----------------		
+					xor r13, r13	;przygotowuje rejestr w ktorym bede skladowal odkodowane dane, budujac z nich znaki.
 
-					mov al, BMP_BYTE_COUNT_FOR_ONE_CHAR;w rejestrze eax znajdowac sie bedzie offset do miejsca w tablicy, do ktorego chce zaladowac porzadany bit(r13b).
+			inner_for:				;petla wyglada w cpp tak: for(int i=1; i<=24; i+=3) poniewaz kodujemy co trzeci bajt pliku BMP -> 1 bajt wiadomosci = 24 bajty pliku bmp.
+						;obliczam index tablicy z bitmapa	
+					movzx rax, BMP_BYTE_COUNT_FOR_ONE_CHAR;w rejestrze eax znajdowac sie bedzie offset do miejsca w tablicy, do ktorego chce zaladowac porzadany bit(r13b).
 					mul r15d							;zgodnie z algorytmem, przemnazam iterator zewnetrznej petli przez stala potrzebna do ustalenia miejsca w tablicy do zakodowania bitu(BMP_BYTE_COUNT_FOR_ONE_CHAR * zewnetrzny_iterator)
 					add eax, ebx						;zgodnie z algorytmem, dodaje wartosc wewnetrznego iteratora(EAX + wewnetrzny_iterator) 
 
 					mov r12b, [RCX + RAX]				;pobieram z rejestru RCX w którym znajduje siê tablica z bitmap¹ okreslone miejsce dzieki rejestrowi eax
-					and r12b, 1							;wykonuje operacje mod2, aby sprawdzic, czy bit musi zostac podmieniany
-									
-									
+					and r12b, 1							;wykonuje operacje mod2, aby sprawdzic, aby sprawdzic wartosc bitu - jesli =1, trzeba dodac 127
 
-
-
-					add bl, 3		
+					cmp r12b, 1						;sprawdzam czy odkodowany bit jest rowny 1, jesli jest, trzeba bedzie oprocz przesuniecia bitowego, dodac 10000000b, a dopiero potem przesunac
+					je do_operations				;skok do podprogramu dekodujacego 
+					
+					cmp bl, 22				;sprawdzam, czy wewnetrzny iterator nie jest przypadkiem rowny 22, poniewaz aby odkodowac bitow potrzebujemy 7 przesuniec bitowych, a nie 8
+					jne bit_right_shift_no
+					
+					continue:
+					
+					add bl, 3								;inkrementuje licznik wewnetrznej funkcji
 					cmp bl, BMP_BYTE_COUNT_FOR_ONE_CHAR
 					jle inner_for	;#2jump if less or equal
+					jmp inner_loop_end	;skacze na koniec inner petli
 
 
+					do_operations:						;proces dekodowania
+					;&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+											;poniewaz kodowalem przesuwajac bity znaku w lewo, teraz kiedy dekoduje, musze robic to w przeciwnym kierunku, czyli w prawo.
+					add r13b, 10000000b	;odkodowany bit jest rowny 1, dlatego musze dodac 1 na poczatek bajtu
+					cmp bl, 22				;sprawdzam, czy wewnetrzny iterator nie jest przypadkiem rowny 22, poniewaz aby odkodowac bitow potrzebujemy 7 przesuniec bitowych, a nie 8
+					jne bit_right_shift		;skok do podprogramu przesuwajacego o 1 w prawo bity rejestru zawierajacego dekodowane slowo 
+					continue_doing_operations:
+					;sub r13b, 10000000b		;musze odjac 1, po przesunieciu, poniewaz liczba 1...b po przesunieciu o 1 w prawo da 11...b. Nie wiadomo czy nastepny bit bedzie rowny 1 czy 0, dlatego tez pozbywam sie 1
+					jmp continue
 
+					bit_right_shift:		;podprogram ktorego zadaniem jest przesuniecie bitowe dekodowanego slowa o 1 w prawo
+					shr r13b, 1				;operacja przesuniecia bitowego dekodowanego slowa o 1 w prawo
+					jmp continue_doing_operations	;powrot 
+
+					bit_right_shift_no:
+					shr r13b, 1
+					jmp continue
+					;&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 ;---------------- INNER FOR ----------------
+	inner_loop_end:
+	mov [R9 + R15], r13b
 
 
-	xor bl, bl			;resetuje licznik wewnetrznej petli for (inner_for)
+	xor rbx, rbx			;resetuje licznik wewnetrznej petli for (inner_for)
 	inc bl				;nastepnie ustawiam go na 1, zgodnie z zalozeniami petli
 	inc r15d			;inkrementuje licznik zewnetrznej petli for (outer_for)
 	cmp r15d, R8D		;porownuje z mEnd
 	jne outer_for	;skacze jesli mniejsze
+
 	
+	pop rsp						; Return rsp from stack
+	pop rdi						; Return rdi from stack 
+	pop rbp						; Return rbp from stack
+	
+	mov eax, 0
+	ret
 ;---------------- OUTER FOR ----------------	
-ret
 decoding ENDP
 
 
@@ -66,8 +100,6 @@ decoding ENDP
 
 encoding PROC	;(char* bmpArr, char* msg, int mEnd)
 
-; 		;zrzucam ze stosu piaty argument = msgEnd	
-
 	;RCX - bmpArr
 	;RDX - msgArr
 	;R8 - mEnd
@@ -76,20 +108,25 @@ encoding PROC	;(char* bmpArr, char* msg, int mEnd)
 	;RCX(bmpArr) | RDX (msgArr) | R8(mEnd) | r14(LSB) | r8 (jest wolne po przypisaniu do r15) | r13 | r12 | RBX(iterator wewnetrznej)
 
 	
-	mov RSI, RDX	;zapisuje wskaznik na tablice z wiadomoscia, aby moc uzywac instrukcji mul, ktora potrzebuje rejestru RDX do poprawnego dzialania
-	xor bl, bl		;licznik "i" zapisany w rejestrze al wewnetrznej petli for, ktora w cpp wyglada tak: for(int i=1; i<=24; i+=3)
+	push rbp					; Save address of previous stack frame
+	push rdi					; Save register destination index
+	push rsp					; Save stack pointer
+
+	mov R9, RDX		;zapisuje wskaznik na tablice z wiadomoscia, aby moc uzywac instrukcji mul, ktora potrzebuje rejestru RDX do poprawnego dzialania
+	xor rbx, rbx		;licznik "i" zapisany w rejestrze al wewnetrznej petli for, ktora w cpp wyglada tak: for(int i=1; i<=24; i+=3)
 	inc bl			;inkrementujê rejestr al, aby docelowo byl rowny 1
 
-	xor r15d,r15d	;licznik zewnetrznej petli for. Petla w cpp wyglada tak: for(int i=0; i<mEnd; i++), gdzie mEnd to wartosc zaladowana do rejestru r15
+	xor r15,r15	;licznik zewnetrznej petli for. Petla w cpp wyglada tak: for(int i=0; i<mEnd; i++), gdzie mEnd to wartosc zaladowana do rejestru r15
 ;---------------- OUTER FOR ----------------		
 outer_for:	
 ;---------------- INNER FOR ----------------		
-					mov r14b, [RSI + R15]	;czytam przy pomocy iteratora zewnetrznej petli (RBX) jeden znak (bajt) z wiadomosci.
+					mov r14b, [R9 + R15]	;czytam przy pomocy iteratora zewnetrznej petli (RBX) jeden znak (bajt) z wiadomosci.
 			inner_for:				;petla wyglada w cpp tak: for(int i=1; i<=24; i+=3) poniewaz kodujemy co trzeci bajt pliku BMP -> 1 bajt wiadomosci = 24 bajty pliku bmp.
 					mov r13b, r14b			;zapisuje r13, aby nie stracic bajtu z r14. Bajt zostanie potraktowany przesunieciem bitowym w lewo o 1 z koncem petli. 
 					and r13b, 1				;wykonujê operacjê mod2, aby odczytaæ bit o najmniejszym znaczeniu bajtu, który nastêpnie zostanie zakodowany w obrazie. 
 
-					mov al, BMP_BYTE_COUNT_FOR_ONE_CHAR;w rejestrze eax znajdowac sie bedzie offset do miejsca w tablicy, do ktorego chce zaladowac porzadany bit(r13b).
+														;obliczam index tablicy z bitmapa	
+					movzx rax, BMP_BYTE_COUNT_FOR_ONE_CHAR;w rejestrze eax znajdowac sie bedzie offset do miejsca w tablicy, do ktorego chce zaladowac porzadany bit(r13b).
 					mul r15d							;zgodnie z algorytmem, przemnazam iterator zewnetrznej petli przez stala potrzebna do ustalenia miejsca w tablicy do zakodowania bitu(BMP_BYTE_COUNT_FOR_ONE_CHAR * zewnetrzny_iterator)
 					add eax, ebx						;zgodnie z algorytmem, dodaje wartosc wewnetrznego iteratora(EAX + wewnetrzny_iterator) 
 
@@ -102,7 +139,7 @@ outer_for:
 					continue:
 					shr r14b, 1
 					
-					add bl, 3		
+					add bl, 3							;inkrementuje licznik wewnetrznej funkcji
 					cmp bl, BMP_BYTE_COUNT_FOR_ONE_CHAR
 					jle inner_for	;#2jump if less or equal
 					jmp inner_loop_end	;skacze na koniec inner petli
@@ -128,7 +165,8 @@ outer_for:
 					cvtsi2ss xmm0, r13d					;laduje zanegowany bit do rejestru uzyskujac ustawienie
 					;movd xmm0, r13d						;laduje zanegowany bit do rejestru uzyskujac ustawienie
 
-														;teraz bede przygotowywal okreslony bajt tablicy dodajac 1 lub odejmujac 1
+						xor r11, r11
+						;teraz bede przygotowywal okreslony bajt tablicy dodajac 1 lub odejmujac 1
 					mov r11b, [RCX + RAX]				;laduje bajt tablicy do zakodowania, w celu przygotowania go do zaladowania do rejestru xmm1
 					add r11b, 00000001b
 					cvtsi2ss xmm1, r11d
@@ -148,11 +186,18 @@ outer_for:
 ;---------------- INNER FOR ----------------
 	inner_loop_end:
 
-	xor bl, bl			;resetuje licznik wewnetrznej petli for (inner_for)
+	xor rbx, rbx			;resetuje licznik wewnetrznej petli for (inner_for)
 	inc bl				;nastepnie ustawiam go na 1, zgodnie z zalozeniami petli
 	inc r15d			;inkrementuje licznik zewnetrznej petli for (outer_for)
 	cmp r15d, R8D		;porownuje z mEnd
 	jne outer_for	;skacze jesli mniejsze
+
+	
+	pop rsp						; Return rsp from stack
+	pop rdi						; Return rdi from stack 
+	pop rbp						; Return rbp from stack
+	
+	mov eax, 0
 	ret
 ;---------------- OUTER FOR ----------------	
 
--- JA_cppAlgo/JA_cppAlgo.cpp
+++ JA_cppAlgo/JA_cppAlgo.cpp
@@ -7,12 +7,11 @@
 
 
 
-void decoding(char* bmpArray,  char* msg, int mEnd)
+void decoding(char* bmpArray, char* msg, int mEnd)
 {
 	short LSB;
 	char letter = 0;
-	bool changed = false;
-	for (int j = 0; j < mEnd ; j++)
+	for (int j = 0; j < mEnd; j++)
 	{
 		for (int i = 1; i <= BMP_BYTE_COUNT_FOR_ONE_CHAR; i += 3)
 		{
@@ -21,21 +20,22 @@ void decoding(char* bmpArray,  char* msg, int mEnd)
 			{
 				//letter = letter || insertOne;
 				letter += 0b10000000;
-				changed = true;
-			}
-			if (i != 22)	//bo przesuwam o 1 raz duzo 
-				letter >>= 1;
-
-			if (changed)
-			{
+				//changed = true;
+				if (i != 22)
+					letter >>= 1;
 				letter -= 0b10000000;
 			}
-			changed = false;
+			else if (i != 22)//bo przesuwam o 1 raz duzo 
+			{
+				letter >>= 1;
+			}
 		}
 		*(msg + j) = letter;
 		letter = 0;
 	}
 }
+
+
 
 void encoding(char* bmpArray, char* msg, int mEnd)
 {
@@ -45,20 +45,18 @@ void encoding(char* bmpArray, char* msg, int mEnd)
 	{
 		for (int i = 1; i <= BMP_BYTE_COUNT_FOR_ONE_CHAR; i += 3)	//i=1, bo zaczynamy kodowac G (BGR)
 		{
-			LSB = msg[ j] % 2;	// czytamy sobie ostatni bit
+			LSB = msg[j] % 2;	// czytamy sobie ostatni bit
 
-			tmp = ((unsigned char)bmpArray[BMP_BYTE_COUNT_FOR_ONE_CHAR * j  + i ]) % 2;
+			tmp = ((unsigned char)bmpArray[BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i]) % 2;
 			if (tmp != LSB)	//jesli mozna pominac zamiane 
 			{
-				unsigned char x = *(bmpArray + (BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i));
 				*(bmpArray + (BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i)) =
 					(*(bmpArray + (BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i)) + 1) * LSB
 					+ (*(bmpArray + (BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i)) - 1) * !LSB;
-				tmp = *(bmpArray + (BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i));
 			}
-			tmp = *(msg  + j);
+			tmp = *(msg + j);
 			tmp >>= 1;			//przesuwamy bity przygotowujac na nastepny bit do czytania]
-			*(msg  + j) = tmp;
+			*(msg + j) = tmp;
 
 		}
 	}
