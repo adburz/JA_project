adbbb1251a85c4a4dad2e44e182fe698160416cc
adburz <adam.burzynski18922@gmail.com>
2020-11-02 23:34:43 +0100

    cpp functions corrected

--- JA_cppAlgo/JA_cppAlgo.cpp
+++ JA_cppAlgo/JA_cppAlgo.cpp
@@ -3,6 +3,7 @@
 #include "JA_cppAlgo.h"
 
 #define BMP_BYTE_COUNT_FOR_ONE_CHAR 24
+#define LAST_ITERATION_I_VALUE 22
 
 
 
@@ -10,28 +11,22 @@
 void decoding(char* bmpArray, char* msg, int mEnd)
 {
 	short LSB;
-	char letter = 0;
+	unsigned char letter = 0;
 	for (int j = 0; j < mEnd; j++)
 	{
 		for (int i = 1; i <= BMP_BYTE_COUNT_FOR_ONE_CHAR; i += 3)
 		{
 			LSB = ((unsigned char)bmpArray[i + j * BMP_BYTE_COUNT_FOR_ONE_CHAR]) % 2;
-			if (LSB == 1)
-			{
-				//letter = letter || insertOne;
-				letter += 0b10000000;
-				//changed = true;
-				if (i != 22)
-					letter >>= 1;
-				letter -= 0b10000000;
-			}
-			else if (i != 22)//bo przesuwam o 1 raz duzo 
+			letter &= 0b01111111;
+			letter |= (0b10000000 * LSB);
+
+
+			if (i != LAST_ITERATION_I_VALUE)	//aby odkodowac 8 bitow, potrzeba 7 przesuniec
 			{
 				letter >>= 1;
 			}
 		}
 		*(msg + j) = letter;
-		letter = 0;
 	}
 }
 
@@ -40,24 +35,16 @@ void decoding(char* bmpArray, char* msg, int mEnd)
 void encoding(char* bmpArray, char* msg, int mEnd)
 {
 	short LSB;
-	unsigned char tmp;
 	for (int j = 0; j < mEnd; j++)
 	{
 		for (int i = 1; i <= BMP_BYTE_COUNT_FOR_ONE_CHAR; i += 3)	//i=1, bo zaczynamy kodowac G (BGR)
 		{
-			LSB = msg[j] % 2;	// czytamy sobie ostatni bit
+			LSB = msg[j] % 2;	// czytamy ostatni bit
 
-			tmp = ((unsigned char)bmpArray[BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i]) % 2;
-			if (tmp != LSB)	//jesli mozna pominac zamiane 
-			{
-				*(bmpArray + (BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i)) =
-					(*(bmpArray + (BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i)) + 1) * LSB
-					+ (*(bmpArray + (BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i)) - 1) * !LSB;
-			}
-			tmp = *(msg + j);
-			tmp >>= 1;			//przesuwamy bity przygotowujac na nastepny bit do czytania]
-			*(msg + j) = tmp;
+			*(bmpArray + (BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i)) = (unsigned char)bmpArray[BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i] & 0b11111110;	//zerujemy LSB
+			*(bmpArray + (BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i)) = (unsigned char)bmpArray[BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i] | LSB;			//ustawiamy porzadana wartosc
 
+			*(msg + j) = *(msg + j) >> 1;	//przesuwamy bity o 1 w prawo, aby przy nastepnym przejsciu petli pobrac czyste wartosci.
 		}
 	}
 }
