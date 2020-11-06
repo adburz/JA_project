68d9529e1c0f2be692e8d3e01200bd2cf5e0cfd2
adburz <adam.burzynski18922@gmail.com>
2020-11-06 22:22:53 +0100

    finally update v3

--- JA_cppAlgo/JA_cppAlgo.cpp
+++ JA_cppAlgo/JA_cppAlgo.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include "JA_cppAlgo.h"
 
-#define BMP_BYTE_COUNT_FOR_ONE_CHAR 24
+#define BMP_BYTE_FOR_CHAR 24
 #define LAST_ITERATION_I_VALUE 22
 
 
@@ -14,15 +14,15 @@ void decoding(char* bmpArray, char* msg, int mEnd)
 	unsigned char letter = 0;
 	for (int j = 0; j < mEnd; j++)
 	{
-		for (int i = 1; i <= BMP_BYTE_COUNT_FOR_ONE_CHAR; i += 3)
+		for (int i = 1; i <= BMP_BYTE_FOR_CHAR; i += 3)
 		{
-			LSB = ((unsigned char)bmpArray[i + j * BMP_BYTE_COUNT_FOR_ONE_CHAR]) % 2;
+			LSB = ((unsigned char)bmpArray[i + j * BMP_BYTE_FOR_CHAR]) % 2;
 			letter &= 0b01111111;	//zerujemy najbardziej znaczacy bit
 			letter |= (0b10000000 * LSB);	//podmieniamy go na porzadana wartosc
 
 
-			if (i != LAST_ITERATION_I_VALUE)	//aby odkodowac 8 bitow, potrzeba 7 przesuniec
-			{
+			if (i != LAST_ITERATION_I_VALUE)	//aby odkodowac 8 bitow, 
+			{									// potrzeba 7 przesuniec
 				letter >>= 1;
 			}
 		}
@@ -37,12 +37,15 @@ void encoding(char* bmpArray, char* msg, int mEnd)
 	short LSB;
 	for (int j = 0; j < mEnd; j++)
 	{
-		for (int i = 1; i <= BMP_BYTE_COUNT_FOR_ONE_CHAR; i += 3)	//i=1, bo zaczynamy kodowac G (BGR)
+		for (int i = 1; i <= BMP_BYTE_FOR_CHAR; i += 3)	//i=1, bo zaczynamy kodowac G (BGR)
 		{
 			LSB = msg[j] % 2;	// czytamy ostatni bit
 
-			*(bmpArray + (BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i)) = (unsigned char)bmpArray[BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i] & 0b11111110;	//zerujemy LSB
-			*(bmpArray + (BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i)) = (unsigned char)bmpArray[BMP_BYTE_COUNT_FOR_ONE_CHAR * j + i] | LSB;			//ustawiamy porzadana wartosc
+			*(bmpArray + (BMP_BYTE_FOR_CHAR * j + i)) 
+				= (unsigned char)bmpArray[BMP_BYTE_FOR_CHAR * j + i] & 0b11111110;	//zerujemy LSB
+
+			*(bmpArray + (BMP_BYTE_FOR_CHAR * j + i)) 
+				= (unsigned char)bmpArray[BMP_BYTE_FOR_CHAR * j + i] | LSB;		//ustawiamy porzadana wartosc
 
 			*(msg + j) = *(msg + j) >> 1;	//przesuwamy bity o 1 w prawo, aby przy nastepnym przejsciu petli pobrac czyste wartosci.
 		}
